ae94729f86bcf14988c912eecd10b08d1b02d4cd
lolotrgeek <jon@simplyenable.com>
2021-12-21 09:23:46 -0600

    feat: array and object filters

--- lib.js
+++ lib.js
@@ -1,20 +1,59 @@
 /**
  * forEach in reverse to walk backward through an array
+ * `array.forEachRev(element => ())`
  */
- Object.defineProperty(Array.prototype, 'forEachRev', {
-    value: function (callback) {
-      for (let i = this.length - 1; i >= 0; i--) {
-        element = this[i]
-        callback(element, i)
-      }
+Object.defineProperty(Array.prototype, 'forEachRev', {
+  value: function (callback) {
+    for (let i = this.length - 1; i >= 0; i--) {
+      element = this[i]
+      callback(element, i)
     }
-  })
-  
-  /**
-   * Get the maximum value of an array of integers
-   * @param {*} array 
-   * @returns 
-   */
-  Array.max = function (array) {
-    return Math.max.apply(Math, array)
-  };
+  }
+})
+
+/**
+ * Get the maximum value of an array of integers
+ * @param {*} array 
+ * @returns 
+ */
+Array.max = function (array) {
+  return Math.max.apply(Math, array)
+}
+
+/**
+* 
+* @param {array} array of objects with a key holding a numerical value
+* @param {string} key of value to find maximum of
+* @returns max value of objects
+*/
+Array.maxObject = function (array, key) {
+  return Math.max.apply(Math, array.map(function (o) { return o[key]; }))
+}
+
+/**
+* 
+* @param {array} array of objects with a key holding a numerical value
+* @param {string} key of value to find maximum of
+* @returns object that contains max value
+*/
+Array.maxObjectValue = function (array, key) {
+  let max = Array.maxObject(array, key)
+  return array.find(o => o[key] === max)
+}
+
+/**
+ * Creates a filtered copy of an object
+ * @param {object} object 
+ * @param {array} allowed list of allowed keys
+ * @returns 
+ */
+ Object.prototype.filter = function (object, allowed) {
+  return Object.keys(object)
+      .filter(key => allowed.includes(key))
+      .reduce((obj, key) => {
+          return {
+              ...obj,
+              [key]: object[key]
+          }
+      }, {})
+}
